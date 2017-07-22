e6df89f4a5458ec1591975b5617ec39a1eb3c20f
jomaf1010 <fontanillajosephjohn@gmail.com>
2017-07-22 15:01:50 +0800

    Command Bus initial files

---
+++ src/CommandBus.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jomaf - Joseph John Fontanilla
+ * Date: 7/22/17
+ * Time: 2:58 PM
+ */
+
+namespace Jomaf\CommandBus;
+
+
+class CommandBus
+{
+
+}
---
+++ src/CommandBusServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jomaf - Joseph John Fontanilla
+ * Date: 7/22/17
+ * Time: 2:59 PM
+ */
+
+namespace Jomaf\CommandBus;
+
+
+use Illuminate\Support\ServiceProvider;
+
+class CommandBusServiceProvider extends ServiceProvider
+{
+
+}
